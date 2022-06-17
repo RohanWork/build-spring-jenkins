1ea3dee313c415c41b291213e4f83c1c8ffa0894
Rohan Katare <rohankatarework@gmail.com>
2022-06-18 00:05:00 +0530

    Initial commit

---
+++ src/main/java/com/rohan/learning/BuildSpringJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildSpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildSpringJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/learning/BuildSpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildSpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
